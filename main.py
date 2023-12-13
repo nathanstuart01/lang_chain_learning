6584d60aca94c26808ee1987c7f773f7e3762c66
Nathan Stuart <nathan.stuart01@gmail.com>
2023-12-12 21:35:01 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import dotenv
+from langchain.llms import OpenAI
+from langchain.utilities import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
+
+dotenv.load_dotenv()
+
+db = SQLDatabase.from_uri("sqlite:///Chinook.db")
+llm = OpenAI(temperature=0, verbose=True)
+db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+
+db_chain.run("How many employees are there?")
