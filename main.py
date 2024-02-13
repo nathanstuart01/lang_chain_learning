86fe11bd0cb7ace3524569efdc5fca626fe571d2
Nathan Stuart <nathan.stuart01@gmail.com>
2024-02-13 06:52:01 -0800

    fix: gathered stats besides def

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 URL = 'https://www.espn.com'
 HEADERS = {
         'User-Agent': 'My User Agent 1.0',
-        'From': 'youremail@domain.example'  # This is another valid field
+        'From': 'youremail@domain.example'
     }
 
 def split_list(list1, chunk_size):
@@ -30,42 +30,33 @@ def sort_table_data(collected_table_data: dict) -> list[dict]:
 
 def collect_table_data(box_score_data: BeautifulSoup, key_name: str, team_index: int) -> List:
     collected_table_data = {}
-    # how to handle the 'No team name Kick Returns' or 'No team name Punt Returns'
-    # just do a check on if 'No' in table data? pass? or return empty array?
     if 'Defensive' not in key_name:
         team_table_data = box_score_data.select('.Boxscore__Team')[team_index]
         team_stats = team_table_data.select('.Table__TBODY')
         collected_table_data['team_data_categories'] = [t.text for t in team_table_data.select('.Table__TH')]
-        collected_table_data['players'] = [t.text for t in team_stats[0]]
-        collected_table_data['stats'] = [t.text for t in team_stats[1].select('.Table__TD')]
+        collected_table_data['players'] = [t.text for t in team_stats[0]] if team_stats else []
+        collected_table_data['stats'] = [t.text for t in team_stats[1].select('.Table__TD')] if team_stats else []
         return sort_table_data(collected_table_data)
     else:
-        print('the defensive table stuff')
         return []
 
 def get_individual_team_boxscores(team_data: BeautifulSoup):
     box_scores = team_data.select('.Boxscore__Category')
     team_1_box_score = {}
     team_2_box_score = {}
-    # just got passing and returned
-    # probably just need to change it to not return and get all stats
     for box_score in box_scores:
         key_name = box_score.select('.TeamTitle__Name')[0].text
         team_1_box_score[key_name] = collect_table_data(box_score, key_name, 0)
-        print(f'team 1 data: {key_name}')
         key_name = box_score.select('.TeamTitle__Name')[1].text
         team_2_box_score[key_name] = collect_table_data(box_score, key_name, 1)
-        print(f'team 2 data: {key_name}')
-        return [team_1_box_score, team_2_box_score]
+    return [team_1_box_score, team_2_box_score]
 
 
-def get_boxscores(df: pd.DataFrame) -> List[dict]:
-    boxscores = []
-    for index, row in df.iterrows():
-        soup = BeautifulSoup(requests.get(URL + row['Box Score Url'],headers=HEADERS).content, 'html.parser')
-        data = soup.select('.Boxscore')[0]
-        boxscores.append(get_individual_team_boxscores(data))
-    return boxscores
+def get_boxscores(url: str) -> List[dict]:
+    soup = BeautifulSoup(requests.get(URL + url, headers=HEADERS).content, 'html.parser')
+    data = soup.select('.Boxscore')[0]
+    return get_individual_team_boxscores(data)
+
 
 def get_games_data(soup: BeautifulSoup, week: int) -> pd.DataFrame:
     all_data = []
@@ -75,20 +66,23 @@ def get_games_data(soup: BeautifulSoup, week: int) -> pd.DataFrame:
     game_number = 0
     for game in games:
         counter += 1
+        box_scores = game_links[game_number].find_all('a', href=True)
+        boxsore_url = box_scores[1]['href']
+        box_scores = get_boxscores(boxsore_url)
         if counter % 2 != 0:
             title = game.find_previous(class_='Card__Header__Title').text
             team1 = [t.text for t in game.select('.ScoreCell__TeamName')][0]
             score1 = [t.text for t in game.select('.ScoreCell__Score')][0] or '-'
             quarter_scores1 = {f'q{i + 1}': int(t.text)  for i, t in enumerate(game.select('.ScoreboardScoreCell__Value'))}
+            box_score_1 = box_scores[0]
             continue
         else:
             team2 = [t.text for t in game.select('.ScoreCell__TeamName')][0]
             score2 = [t.text for t in game.select('.ScoreCell__Score')][0] or '-'
             quarter_scores2 = {f'q{i + 1}': int(t.text)  for i, t in enumerate(game.select('.ScoreboardScoreCell__Value'))}
-            boxscores = game_links[game_number].find_all('a', href=True)
-            boxsore_url = boxscores[1]['href']
+            box_score_2 = box_scores[1]
             game_number += 1
-            all_data.append((week, title, team1, score1, quarter_scores1, team2, score2, quarter_scores2, boxsore_url))
+            all_data.append((week, title, team1, score1, quarter_scores1, team2, score2, quarter_scores2, box_score_1, box_score_2))
             continue
     
     return pd.DataFrame(all_data, columns=[
@@ -100,13 +94,12 @@ def get_games_data(soup: BeautifulSoup, week: int) -> pd.DataFrame:
         'Team 2', 
         'Score 2',
         'Quarter Scores Team 2',
-        'Box Score Url'])
+        'Team 1 Box Score',
+        'Team 2 Box Score'])
 
 
 if __name__ == '__main__':
     week = 18
     soup = BeautifulSoup(requests.get(f'{URL}/nfl/scoreboard/_/week/18/year/2023/seasontype/2', headers=HEADERS).content, 'html.parser')
     df = get_games_data(soup, week)
-    boxscores = get_boxscores(df)
-    import pdb;pdb.set_trace()
     print('done')
